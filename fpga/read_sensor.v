cddee66b782a2ec8c2375759353642b12ea2311d
samarqureshii <samarq713@gmail.com>
2023-11-29 20:33:15 -0500

    Cleaning up, fixed GPIO code

--- fpga/read_sensor.v
+++ fpga/read_sensor.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ns
 
-module toplevel(GPIO_0, LEDR);
-    input [0:0] GPIO_0;  
+module toplevel(GPIO_1, LEDR);
+    input [0:0] GPIO_1;  
     output [9:0] LEDR;  
 
     // first three LEDs to the GPIO inputs so we know the binary value
-    assign LEDR[0] = GPIO_0[0];
-    assign LEDR[1] = GPIO_1[0];
-    assign LEDR[2] = GPIO_2[0];
+    assign LEDR[0] = GPIO_1[0];
+    assign LEDR[1] = GPIO_1[1];
+    assign LEDR[2] = GPIO_1[2];
 
     // remaining LEDs are turned off
     assign LEDR[9:3] = 7'b0;
