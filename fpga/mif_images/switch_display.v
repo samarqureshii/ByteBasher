50906880c531b34b89ba5ad18182a6fa3e81f69d
anniew5 <annniewanng@gmail.com>
2023-12-01 19:18:59 -0500

    Update switch_display.v

--- fpga/mif_images/switch_display.v
+++ fpga/mif_images/switch_display.v
@@ -1,11 +1,6 @@
-//
-// This is the template for Part 2 of Lab 7.
-//
-// Paul Chow
-// November 2021
-//
 
-module part2(
+
+module switch_display(
             /*************INPUTS************/
             reset, //active low synchronous reset 
             iPlotBox, //When pulsed (goes high then low), it triggers the circuit to start drawing the square with the specified colour at the specified coordinates 
@@ -183,21 +178,76 @@ module Datapath(
                 counter <= 0;
             end
 			
-			if(current_level == 2'b00)
+			if(current_level == 2'b01)
         	begin
-			colour <= colour0;
+			colour <= static;
         	end
-	
-        	else if(current_level == 2'b01)
-       		begin
-            colour <= avani_col;
-        	end
-        	else if(current_level == 2'b10)
-        	begin
-            colour <= colour2;
-        	end
+
         end
-    end
+
+
+endmodule
+
+`timescale 1 ps / 1 ps
+// synopsys translate_on
+module static (
+	address,
+	clock,
+	q);
+
+	input	[14:0]  address;
+	input	  clock;
+	output	[2:0]  q;
+`ifndef ALTERA_RESERVED_QIS
+// synopsys translate_off
+`endif
+	tri1	  clock;
+`ifndef ALTERA_RESERVED_QIS
+// synopsys translate_on
+`endif
+
+	wire [2:0] sub_wire0;
+	wire [2:0] q = sub_wire0[2:0];
+
+	altsyncram	altsyncram_component (
+				.address_a (address),
+				.clock0 (clock),
+				.q_a (sub_wire0),
+				.aclr0 (1'b0),
+				.aclr1 (1'b0),
+				.address_b (1'b1),
+				.addressstall_a (1'b0),
+				.addressstall_b (1'b0),
+				.byteena_a (1'b1),
+				.byteena_b (1'b1),
+				.clock1 (1'b1),
+				.clocken0 (1'b1),
+				.clocken1 (1'b1),
+				.clocken2 (1'b1),
+				.clocken3 (1'b1),
+				.data_a ({3{1'b1}}),
+				.data_b (1'b1),
+				.eccstatus (),
+				.q_b (),
+				.rden_a (1'b1),
+				.rden_b (1'b1),
+				.wren_a (1'b0),
+				.wren_b (1'b0));
+	defparam
+		altsyncram_component.address_aclr_a = "NONE",
+		altsyncram_component.clock_enable_input_a = "BYPASS",
+		altsyncram_component.clock_enable_output_a = "BYPASS",
+		altsyncram_component.init_file = "./bmp2mif/bmp2mif/static.mif",
+		altsyncram_component.intended_device_family = "Cyclone V",
+		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
+		altsyncram_component.lpm_type = "altsyncram",
+		altsyncram_component.numwords_a = 19200,
+		altsyncram_component.operation_mode = "ROM",
+		altsyncram_component.outdata_aclr_a = "NONE",
+		altsyncram_component.outdata_reg_a = "UNREGISTERED",
+		altsyncram_component.widthad_a = 15,
+		altsyncram_component.width_a = 3,
+		altsyncram_component.width_byteena_a = 1;
 
 
 endmodule
