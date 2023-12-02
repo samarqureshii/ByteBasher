45dbe8e84491fe77b1b51ac2a53ccc3bf7554435
samarqureshii <samarq713@gmail.com>
2023-12-02 16:09:31 -0500

    seeing if read sensor is outputting correct value

--- fpga/read_sensor.v
+++ fpga/read_sensor.v
@@ -1,9 +1,10 @@
 `timescale 1ns / 1ns
 
-module read_sensor(GPIO_1, LEDR, box_address);
+module read_sensor(GPIO_1, LEDR, box_address, HEX0);
     input [2:0] GPIO_1;  // Declare GPIO_1 as a 3-bit input
     output [2:0] LEDR;  
     output[2:0] box_address;
+    output [6:0] HEX0;
 
     // first three LEDs to the GPIO inputs so we know the binary value
     assign LEDR[0] = GPIO_1[0];
@@ -12,4 +13,25 @@ module read_sensor(GPIO_1, LEDR, box_address);
 
     assign box_address = GPIO_1;
 
+    assign hex_input = {1'b0, box_address};  // Adjusted for 3-bit box_mapped
+    hex_decoder samar(hex_input, HEX0);
+
+endmodule
+
+module hex_decoder(c, display);
+    input [3:0] c;
+    output [6:0] display;
+    
+    assign c0 = c[0];
+    assign c1 = c[1];
+    assign c2 = c[2];
+    assign c3 = c[3];
+
+    assign display[0] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & c2 & ~c1 & ~c0) + (c3 & ~c2 & c1 & c0) + (c3 & c2 & ~c1 & c0);
+    assign display[1] = (~c3 & c2 & ~c1 & c0) + (~c3 & c2 & c1 & ~c0) + (c3 & ~c2 & c1 & c0) + (c3 & c2 & ~c1 & ~c0) + (c3 & c2 & c1 & ~c0) + (c3 & c2 & c1 & c0);
+    assign display[2] = (~c3 & ~c2 & c1 & ~c0) + (c3 & c2 & ~c1 & ~c0) + (c3 & c2 & c1 & ~c0) + (c3 & c2 & c1 & c0); 
+    assign display[3] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & c2 & ~c1 & ~c0) + (~c3 & c2 & c1 & c0) + (c3 & ~c2 & ~c1 & c0) + (c3 & ~c2 & c1 & ~c0) + (c3 & c2 & c1 & c0);
+    assign display[4] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & ~c2 & c1 & c0) + (~c3 & c2 & ~c1 & ~c0) + (~c3 & c2 & ~c1 & c0) + (~c3 & c2 & c1 & c0) + (c3 & ~c2 & ~c1 & c0);
+    assign display[5] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & ~c2 & c1 & ~c0) + (~c3 & ~c2 & c1 & c0) + (~c3 & c2 & c1 & c0) + (c3 & c2 & ~c1 & c0);
+    assign display[6] = (~c3 & ~c2 & ~c1 & ~c0) + (~c3 & ~c2 & ~c1 & c0) + (~c3 & c2 & c1 & c0) + (c3 & c2 & ~c1 & ~c0);
 endmodule
