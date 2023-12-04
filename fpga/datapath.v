279f610e434b9e31ea86c14c37ad6ce43db1f25b
samarqureshii <samarq713@gmail.com>
2023-12-04 02:34:12 -0500

    attempting to not use audio_main

--- fpga/datapath.v
+++ fpga/datapath.v
@@ -1,5 +1,5 @@
     module Datapath(
-        input reset,
+        input resetn,
         input start_game, // Signal to start the game
         input hit_detected, // Signal from FSM when a hit is detected
         input [2:0] sensor_input, // Input from read_sensor()
@@ -75,20 +75,98 @@
 
 
 
-    audio_main audio_unit1( //audio controller for 
-        .CLOCK_50(CLOCK_50), 
-        .KEY(KEY[1]), 
-        .AUD_ADCDAT(AUD_ADCDAT), 
-        .AUD_BCLK(AUD_BCLK), 
-        .AUD_ADCLRCK(AUD_ADCLRCK), 
-        .AUD_DACLRCK(AUD_DACLRCK), 
-        .FPGA_I2C_SDAT(FPGA_I2C_SDAT),
-        .AUD_XCK(AUD_XCK), 
-        .AUD_DACDAT(AUD_DACDAT), 
-        .FPGA_I2C_SCLK(FPGA_I2C_SCLK), 
-        .audio_en(audio_en), 
-        .play_sound(play_sound)
-    );
+    wire				audio_in_available;
+wire		[31:0]	left_channel_audio_in;
+wire		[31:0]	right_channel_audio_in;
+wire				read_audio_in;
+wire [5:0]	audio_from_ram;
+wire				audio_out_allowed;
+wire		[31:0]	left_channel_audio_out;
+wire		[31:0]	right_channel_audio_out;
+wire				write_audio_out;
+wire [17:0] address_count;
+
+
+reg [17:0] addr_count, soundstart, soundend;
+reg [10:0] clock_count;
+localparam winstart = 18'd0,
+			  winend = 18'd16395,
+			  moostart = 18'd16396,
+			  mooend = 18'd66982,
+			  detectstart = 18'd66983, 
+			  detectend = 18'd83254,
+			  cheerstart = 18'd83255,
+			  cheerend = 18'd137138;
+			  
+always @(posedge CLOCK_50) begin
+	if(play_sound == 1'b1 )begin //if we have the correct hit 
+
+        soundstart <= winstart;
+        soundend <= winend;
+
+        // Existing logic to cycle through audio addresses
+        if (clock_count == 11'd1200) begin
+            if (addr_count == soundend) begin 
+                addr_count <= soundstart;
+            end else if ((addr_count >= soundstart) && (addr_count < soundend)) begin
+                addr_count <= addr_count + 1'b1;
+                clock_count <= 0;
+            end else addr_count <= soundstart;
+        end else clock_count <= clock_count + 1;
+    end
+
+	else begin //when play sound is not 1
+        // Reset address count when play_sound is inactive
+        addr_count <= 18'b0;
+        clock_count <= 11'b0;
+    end 
+
+    if(~resetn) begin
+        addr_count <= 18'b0;
+        clock_count <= 11'b0;
+    end
+end
+
+
+assign address_count = addr_count;
+
+
+assign read_audio_in			= audio_in_available & audio_out_allowed;
+assign left_channel_audio_out = {audio_from_ram, 26'b0};
+assign right_channel_audio_out = 32'b0;
+assign write_audio_out			= audio_in_available & audio_out_allowed;
+
+ 
+win_rom ram(.address(address_count), .clock(CLOCK_50), .q(audio_from_ram));
+
+Audio_Controller Audio_Controller (
+	.CLOCK_50					(CLOCK_50),
+	.reset						(~resetn),
+	.clear_audio_in_memory	(),
+	.read_audio_in				(read_audio_in),
+	.clear_audio_out_memory		(),
+	.left_channel_audio_out		(left_channel_audio_out),
+	.right_channel_audio_out	(right_channel_audio_out),
+	.write_audio_out			(audio_en),
+	.AUD_ADCDAT					(AUD_ADCDAT),
+	.AUD_BCLK					(AUD_BCLK),
+	.AUD_ADCLRCK				(AUD_ADCLRCK),
+	.AUD_DACLRCK				(AUD_DACLRCK),
+	.audio_in_available			(audio_in_available),
+	.left_channel_audio_in		(left_channel_audio_in),
+	.right_channel_audio_in		(right_channel_audio_in),
+	.audio_out_allowed			(audio_out_allowed),
+	.AUD_XCK					(AUD_XCK),
+	.AUD_DACDAT					(AUD_DACDAT)
+
+);
+
+avconf #(.USE_MIC_INPUT(1)) avc (
+	.FPGA_I2C_SCLK					(FPGA_I2C_SCLK),
+	.FPGA_I2C_SDAT					(FPGA_I2C_SDAT),
+	.CLOCK_50					(CLOCK_50),
+	.reset						(~resetn)
+);
 
     // audio_start audio_unit2 (
     //         .CLOCK_50(CLOCK_50),
@@ -141,8 +219,8 @@
 
 
     // // Game logic
-    always @(posedge CLOCK_50 or posedge reset) begin
-        if (reset) begin //when we click KEY[0], transition to the reset state 
+    always @(*) begin
+        if (resetn) begin //when we click KEY[0], transition to the reset state 
             //score <= 0;
             //counter <= 0;
             //game_timer <= 6'd0; // Reset game_timer for next game
